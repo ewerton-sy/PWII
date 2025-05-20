85640f07df828b5aa9e91b2aa02768a775fe43d0
ewerton-sy <ewertonlamb@gmail.com>
2025-05-20 16:21:48 -0300

    Navbar adicionado

--- PDO_CRUD/public/usuario/read.php
+++ PDO_CRUD/public/usuario/read.php
@@ -4,13 +4,18 @@
     $stmt = $pdo->query('SELECT*FROM usuario');
     $usuarios = $stmt->fetchAll();
 
+     if( isset($_GET['id'])){
+      $id = $_GET
+      echo "O id $id foi removido"
+     }
+
     // echo '<pre>';
     // echo var_dump($usuarios);
 ?>
   <?php  foreach ($usuarios as $indice => $user) {?>
     <p>
-        <b>Nome da Heroina:</b> <?php echo $user['username'];?>
-        <a href="">Remover</a>
+        <p><b>Nome da Heroina:</b> <?php echo $user['username'];?></p>
+        <a href="http://localhost/PWII/PDO_CRUD/public/usuario/delete.php?id=<?php echo $user["id"]?>">Remover</a>
         <a href="">Editar</a>
     </p>
     <hr>
